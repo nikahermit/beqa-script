19a59a9fedf2f2b99fb3bf14b17620c2e04667b3
nikahermit <nikahermit@itvet.ge>
2025-04-06 14:13:45 +0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,12 +4,12 @@ temp = random.randint(-10, 40)
 is_sunny = True
 
 if temp >= 25 and is_sunny:
-    prignt("it is HOT outside")
-    prignt("it is sunny")
+    print("it is HOT outside")
+    print("it is sunny")
 
 elif temp <= 0 and is_sunny:
-    pringt("it is cold outside")
-    prinht("it is sunny outside")
+    print("it is cold outside")
+    print("it is sunny outside")
 else:
-    prhint("it is neither cold nor hot outside")
-    prinht("it is relatively sunny outside")
+    print("it is neither cold nor hot outside")
+    print("it is relatively sunny outside")
